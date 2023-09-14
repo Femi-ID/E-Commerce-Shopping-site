f002bb04b3e810c237602753fa93a1f12ad1d89e
Femi-ID <104504319+Femi-ID@users.noreply.github.com>
2023-09-14 23:45:01 +0100

    Update changres in the general urls

--- myshop/urls.py
+++ myshop/urls.py
@@ -21,6 +21,9 @@ from django.conf.urls.i18n import i18n_patterns
 from django.utils.translation import gettext_lazy as _
 from django.views.decorators.cache import cache_control
 from django.contrib.staticfiles.views import serve
+# from django.views.generic.base import RedirectView
+
+# favicon_view = RedirectView.as_view(url='static/favicon.ico', permanent=True)
 
 urlpatterns = i18n_patterns(
     path(_('admin/'), admin.site.urls),
